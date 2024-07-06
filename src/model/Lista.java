43e91087484e608a11ce3390d383aec0cf0ac409
Juliane Hennerich <133507690+Julianehennerich@users.noreply.github.com>
2024-07-06 11:42:35 -0300

    atualização lista

--- src/model/Lista.java
+++ src/model/Lista.java
@@ -1,18 +1,19 @@
 package model;
 
+import java.util.List;
+
 public class Lista {
 	
 	private Long idLista;
 	private String nome;
-	private String listacartao;
-		
+	private List < String > listacartao;
 	
-	public Lista(String nome, String listacartao) {
+		public Lista(String nome, List<String> listacartao) {
 		super();
 		this.nome = nome;
 		this.listacartao = listacartao;
-		
 	}
+		
 	public Long getIdLista() {
 		return idLista;
 	}
@@ -25,11 +26,11 @@ public class Lista {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public String getListacartao() {
+	public List<String> getListacartao() {
 		return listacartao;
 	}
-	public void setListacartao(String listacartao) {
+	public void setListacartao(List<String> listacartao) {
 		this.listacartao = listacartao;
 	}
-	
+			
 }
